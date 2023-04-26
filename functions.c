c408b8e43b57a9dd27cdba08e52e7fc760539c60
jkkaranja <karanjohnk77@gmail.com>
2023-04-25 17:19:10 -0700

    Task 1

---
+++ functions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - entry point of the prog
+  *
+  * Return: 0 on success
+  */
+int main(void)
+{
+	int number = 24;
+
+	printf("%d\n", num);
+
+	printf("%i\n", num);
+
+	return (0);
+}
